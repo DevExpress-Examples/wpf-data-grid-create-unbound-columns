40b4e1968f7b2c1f3c0e42dac4403cd735e1545a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_UnboundColumns/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+
+namespace DXGrid_UnboundColumns {
+    #region #1 Window1 class
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.ItemsSource = new dsProductsTableAdapters.ProductsTableAdapter().GetData();
+        }
+
+        private void grid_CustomUnboundColumnData(object sender, GridColumnDataEventArgs e) {
+            if (e.IsGetData) {
+                int price = Convert.ToInt32(e.GetListSourceFieldValue("UnitPrice"));
+                int unitsOnOrder = Convert.ToInt32(e.GetListSourceFieldValue("UnitsOnOrder"));
+                e.Value = price * unitsOnOrder;
+            }
+        }
+    }
+    #endregion #1
+}
---
+++ VB/DXGrid_UnboundColumns/Window1.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace DXGrid_UnboundColumns
+	#Region "#1 Window1 class"
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New dsProductsTableAdapters.ProductsTableAdapter().GetData()
+		End Sub
+
+		Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, _
+				ByVal e As DevExpress.Xpf.Grid.GridColumnDataEventArgs)
+			If e.IsGetData Then
+				Dim price As Integer = Convert.ToInt32(e.GetListSourceFieldValue("UnitPrice"))
+				Dim unitsOnOrder As Integer = _
+					Convert.ToInt32(e.GetListSourceFieldValue("UnitsOnOrder"))
+				e.Value = price * unitsOnOrder
+			End If
+		End Sub
+	End Class
+	#End Region ' #1
+End Namespace
