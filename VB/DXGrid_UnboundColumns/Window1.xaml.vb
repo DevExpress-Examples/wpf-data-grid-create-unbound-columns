a180847ad991241b4f5ab38d5eac3d382e12a47d
DevExpressExampleBot <fakeEmail>
2023-02-22 19:16:59 +0400

    Source auto update [skip ci]

--- VB/DXGrid_UnboundColumns/Window1.xaml.vb
+++ VB/DXGrid_UnboundColumns/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to create unbound columns
 ' 
 ' This example shows how to add an unbound column to the DXGrid control. This
@@ -7,29 +7,19 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E1503
-
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Grid
 
 Namespace DXGrid_UnboundColumns
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = (New dsProductsTableAdapters.ProductsTableAdapter()).GetData()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = New dsProductsTableAdapters.ProductsTableAdapter().GetData()
         End Sub
 
         Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As GridColumnDataEventArgs)
@@ -40,6 +30,4 @@ Namespace DXGrid_UnboundColumns
             End If
         End Sub
     End Class
-
-
 End Namespace
